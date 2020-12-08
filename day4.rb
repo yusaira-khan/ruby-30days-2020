22ed12d0d817f934342bbdd68ce6edfcba9e78d8
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2020-12-08 09:45:35 -0500

    add impl (untested) to class

--- day4.rb
+++ day4.rb
@@ -1,13 +1,24 @@
 class Person
   attr_accessor :age
   def initialize(initialAge)
-    # Add some more code to run some checks on initialAge
+    @age = initialAge
+    if initialAge < 0
+      puts 'Age is not valid, setting age to 0.'
+      @age = 0
+    end
   end
   def amIOld()
-    # Do some computations in here and print out the correct statement to the console
+    if @age < 13
+      puts 'You are young.'
+    elsif @age < 18
+      puts 'You are a teenager.'
+    else
+      puts 'You are old.'
+    end
   end
+
   def yearPasses()
-    # Increment the age of the person in here
+    @age += 1
   end
 end
 
